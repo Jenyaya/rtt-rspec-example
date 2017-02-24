15822f0a0245871fbae6e6f612758e4d8d848bd5
Yevgeniy Nosenko <yevgeniy.nosenko@avid.com>
2017-02-24 17:33:02 +0200

    added app

--- app/car.rb
+++ app/car.rb
@@ -1,10 +1,13 @@
 class Car
-attr_accessor :brand, :model
+  attr_accessor :brand, :model
 
-  def initalize()
+  def initialize()
     @brand = 'toyota'
     @model = 'corolla'
   end
 
+  def is_brand?(brand)
+    brand.eql? @brand
+  end
 
 end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -2,8 +2,22 @@ require 'init'
 
 describe Car do
 
+  let (:car) { Car.new }
+
   it 'has method brand' do
     expect(Car.new).to respond_to(:brand)
+  end
+
+  it :brand= do
+    expect(Car.new).to respond_to(:brand=)
+  end
+
+  it 'car is Toyota' do
+    expect(car.is_brand?('toyota')).to be true
+  end
+
+  it 'car is not Honda' do
+    expect(car.is_brand?('honda')).to be false
   end
 
 end
